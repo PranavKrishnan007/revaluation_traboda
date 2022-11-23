42d7cf83b0b584afbd3f772b9fdff75c84a39b8b
Pranavkrishnan007 <pranavk0217@gmail.com>
2022-11-23 23:05:13 +0530

    navbar completed

--- copy_ui/pages/index.tsx
+++ copy_ui/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavBar from "../src/components/navbar";
+import SideBar from "../src/components/sidebar";
 
 export default function Home() {
   return (
-      <div>
+      <div className="flex flex-col">
           <NavBar/>
+          <SideBar/>
           {/*<ResponsiveDrawer/>*/}
           {/*<div className="p-7"><h1>home page</h1></div>*/}
           {/*/!*<div className="flex-none">*!/*/}
