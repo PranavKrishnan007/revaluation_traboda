ee10838f37f79e2bbc32c8eebf803496084dc747
Pranavkrishnan007 <pranavk0217@gmail.com>
2022-11-26 00:07:21 +0530

    tailwind scrollbar hiding

--- copy_ui/pages/index.tsx
+++ copy_ui/pages/index.tsx
@@ -5,7 +5,7 @@ import MainHead from "../src/components/mainhead";
 
 export default function Home() {
   return (
-      <div className="flex flex-col h-screen">
+      <div className="flex flex-col h-screen overflow-hidden">
           <NavBar/>
           <div className="flex flex-row">
               <SideBar/>
--- copy_ui/tailwind.config.js
+++ copy_ui/tailwind.config.js
@@ -18,5 +18,7 @@ module.exports = {
       filter: ['hover']
     },
   },
-  plugins: [],
+  plugins: [
+    require('tailwind-scrollbar-hide'),
+  ],
 };
